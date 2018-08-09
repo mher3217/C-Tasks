21135438424a268ee7fc42f4031ae1a37248261c
mher3217 <mher-harutyunyan92@mail.ru>
2018-08-09 14:31:42 +0400

    Add files via upload

---
+++ new_tasks/recurs_revers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+const int Size = 5;
+
+
+void rec(int *p, int k, int e){
+  if (k > 2){
+    int tmp = *(p+e);
+    *(p + e) = *(p + k);
+    *(p + k) = tmp;    
+    rec(p, --k, ++e);  
+  }
+  else{ 
+    return;
+  } 
+}
+
+int main() {
+  
+  int arr[Size] = {16, 7, 4, 63, 1};
+  int k = Size - 1;
+  rec(arr, k, 0);
+  
+  for(int i = 0; i < Size; i++){
+    std::cout << arr[i] << std::endl;
+  }
+  
+
+  return 0;
+}
